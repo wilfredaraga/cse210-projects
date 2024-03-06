4659800b380158dac4e6c9c93ed2f5784514c875
wilfredaraga <wilfredeiron@gmail.com>
2024-03-06 18:04:18 +0800

    W02 Prepare: Learning Activity

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+
+        Job job1 = new Job();
+        job1._company = "Google";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 2018;
+        job1._endYear = 2019;
+        
+        Job job2 = new Job();
+        job2._company = "Facebook";
+        job2._jobTitle = "Social Media Manager";
+        job2._startYear = 2019;
+        job2._endYear = 2021;
+
+        Resume applicant = new Resume();
+        applicant._name = "Eiron Araga";
+        applicant._jobs.Add(job1);
+        applicant._jobs.Add(job2);
+        applicant.Display();
+
+
+
     }
 }
